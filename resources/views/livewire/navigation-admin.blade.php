cb8604fa9b2b02134e6b1e97c87d092afc3171ed
tamam <risda123tamam@gmail.com>
2022-06-27 12:00:59 +0700

    Change route profile to setting

--- resources/views/livewire/navigation-admin.blade.php
+++ resources/views/livewire/navigation-admin.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('admin-dashboard') }}">
+                    <a href="/">
                         <x-jet-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
@@ -132,8 +132,8 @@
                                 {{ __('Home') }}
                             </x-jet-dropdown-link>
 
-                            <x-jet-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
+                            <x-jet-dropdown-link href="{{ route('profile.edit') }}">
+                                {{ __('Setting') }}
                             </x-jet-dropdown-link>
 
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
@@ -212,8 +212,8 @@
                     {{ __('Home') }}
                 </x-jet-responsive-nav-link> 
 
-                <x-jet-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-                    {{ __('Profile') }}
+                <x-jet-responsive-nav-link href="{{ route('profile.edit') }}" :active="request()->routeIs('profile.edit')">
+                    {{ __('Setting') }}
                 </x-jet-responsive-nav-link>
 
                 @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
