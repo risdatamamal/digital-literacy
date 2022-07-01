995fd5b69ae01ac96c49eb84cced87df960b4451
diaansit <diansitanggang161201@gmail.com>
2022-07-02 01:06:09 +0700

    Update logo in dashboard

--- resources/views/pages/user/dashboard.blade.php
+++ resources/views/pages/user/dashboard.blade.php
@@ -11,7 +11,11 @@
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
                     <div>
                         <x-jet-application-logo class="block h-12 w-auto" />
+                        <div>
+                            <h3>Digital Literacy</h3>
+                        </div>
                     </div>
+                    
                 </div>
 
                 <div class="bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
