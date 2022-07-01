0250c8a1b28d71f03d64df5a7dee6d83bcbed0ef
nadillasyaharani <ndlsyhrn@gmail.com>
2022-07-01 18:08:32 +0800

    Change user dashboard main page

--- app/Http/Controllers/user/DashboardController.php
+++ app/Http/Controllers/user/DashboardController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers\user;
 
+use Illuminate\Support\Facades\Auth;
+use App\Models\Book;
+use App\Models\Article;
+use App\Models\Quote;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index() 
     {
-        return view('pages.user.dashboard');
+        $data = [
+            'books' => Book::where('user_id',Auth::user()->id)->get(),
+            'articles' => Article::where('user_id',Auth::user()->id)->get(),
+            'quotes' => Quote::where('user_id',Auth::user()->id)->get()
+        ];
+        // dd($data);
+        return view('pages.user.dashboard',$data);
     }
 }
